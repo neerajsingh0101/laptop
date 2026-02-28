63ce8b6ea1a859dc51a44d0eb6a5ea95cda8cf60
Neeraj Singh <neeraj@bigbinary.com>
2026-02-28 11:37:08 -0500

    Add wezterm keybinding to split 2x2 and run start_server/ui/bg/tunnel2

--- symlinks/.wezterm.lua
+++ symlinks/.wezterm.lua
@@ -117,6 +117,40 @@ config.keys = {
     mods = 'SUPER',
     action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
   },
+    -- 2x2 grid and run commands in each pane (Shift+Opt+4)
+  {
+  key = '$',
+  mods = 'SHIFT|OPT',
+  action = wezterm.action_callback(function(window, pane)
+    local act = wezterm.action
+
+    -- 1) Top-left (current)
+    pane:send_text("start_server\n")
+
+    -- 2) Split right -> top-right becomes active
+    window:perform_action(act.SplitHorizontal { domain = 'CurrentPaneDomain' }, pane)
+    local top_right = window:active_pane()
+    top_right:send_text("start_ui\n")
+
+    -- 3) Go back to left pane, split down -> bottom-left becomes active
+    window:perform_action(act.ActivatePaneDirection 'Left', top_right)
+    local top_left = window:active_pane()
+    window:perform_action(act.SplitVertical { domain = 'CurrentPaneDomain' }, top_left)
+    local bottom_left = window:active_pane()
+    bottom_left:send_text("start_bg\n")
+
+    -- 4) Go to top-right pane, split down -> bottom-right becomes active
+    window:perform_action(act.ActivatePaneDirection 'Right', bottom_left)
+    local top_right_again = window:active_pane()
+    window:perform_action(act.SplitVertical { domain = 'CurrentPaneDomain' }, top_right_again)
+    local bottom_right = window:active_pane()
+    bottom_right:send_text("tunnel2\n")
+
+    -- Optional: focus top-left at the end
+    window:perform_action(act.ActivatePaneDirection 'Left', bottom_right)
+    window:perform_action(act.ActivatePaneDirection 'Up', window:active_pane())
+  end),
+  },
   -- Rename tab
   {
     key = 'r',
